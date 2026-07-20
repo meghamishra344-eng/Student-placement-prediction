e5e771cfee29f272118b1abc70fd4d68e356989f
meghamishra344-eng <meghamishra344@gmail.com>
2026-07-21 00:10:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import pandas as pd
 import numpy as np
 import joblib
 import plotly.graph_objects as go
-pip install streamlit pandas numpy plotly scikit-learn joblib
 
 # ---------------- PAGE CONFIG ---------------- #
 

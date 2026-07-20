1769dedd7800bd6f4210e541a92b97ac7a144eed
meghamishra344-eng <meghamishra344@gmail.com>
2026-07-21 02:26:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ and a SHAP explanation.
 import streamlit as st
 import pandas as pd
 import numpy as np
-import shap
 import matplotlib.pyplot as plt
 
 from sklearn.compose import ColumnTransformer, make_column_selector
